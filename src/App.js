8d964506e63fb8d1c10f256a4413e490d23f46d6
brijesh <meetbrij@gmail.com>
2019-12-14 20:52:20 +0800

    react app commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="desc">Boseong Nok Chha - Green Treen plantation - in South Korea.</div>
+        <img src="https://breeze-test-version-control.s3.amazonaws.com/boseong_nokcha_photo.jpg" className="App-logo" alt="logo" />
+        <p className="caption">
+          @Source: Photography by Brijesh Bolar.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
